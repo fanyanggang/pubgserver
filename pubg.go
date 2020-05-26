8e88e49858f9aab4990678aacd3518c7da3a0df5
roryfan <roryfan@tencent.com>
2020-05-26 23:41:05 +0800

    update

--- pubg.go
+++ pubg.go
@@ -12,13 +12,13 @@ func main() {
 
 	pubg := pubgsql.SQLGroupConfig{
 		Name:   "pubg",
-		Master: "admin:pubg.2020@tcp(pubg-db.cv3gvs0tnix0.ap-south-1.rds.amazonaws.com:3306)/pubg?charset=utf8&max_idle=100&max_active=500&max_lifetime_sec=1800",
+		Master: "pubg_rw:BcPIzg42B/qko9akLJFi5H0cDn4=@tcp(rummy-allinone.c9jqvxqnoxoi.ap-south-1.rds.amazonaws.com:3306)/pubg?charset=utf8&max_idle=100&max_active=500&max_lifetime_sec=1800",
 		Slaves: []string{"admin:pubg.2020@tcp(pubg-db.cv3gvs0tnix0.ap-south-1.rds.amazonaws.com:3306)/pubg?charset=utf8&max_idle=100&max_active=500&max_lifetime_sec=1800"},
 	}
 
 	financial := pubgsql.SQLGroupConfig{
 		Name:   "financial",
-		Master: "admin:pubg.2020@tcp(pubg-db.cv3gvs0tnix0.ap-south-1.rds.amazonaws.com:3306)/financial?charset=utf8&max_idle=100&max_active=500&max_lifetime_sec=1800",
+		Master: "pubg_rw:BcPIzg42B/qko9akLJFi5H0cDn4=@tcp(rummy-allinone.c9jqvxqnoxoi.ap-south-1.rds.amazonaws.com:3306)/financial?charset=utf8&max_idle=100&max_active=500&max_lifetime_sec=1800",
 		Slaves: []string{"admin:pubg.2020@tcp(pubg-db.cv3gvs0tnix0.ap-south-1.rds.amazonaws.com:3306)/financial?charset=utf8&max_idle=100&max_active=500&max_lifetime_sec=1800"},
 	}
 
@@ -31,7 +31,7 @@ func main() {
 	if model.CONTROL {
 		game.InitRedis("127.0.0.1:6379")
 	} else {
-		game.InitRedis("redis-pubg.upnezx.ng.0001.aps1.cache.amazonaws.com:6379")
+		game.InitRedis("rummy-redis.oigiga.ng.0001.aps1.cache.amazonaws.com:6379")
 	}
 	log.InitLog()
 	game.InitService()
